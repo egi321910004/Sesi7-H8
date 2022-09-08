a01973ffeb5c6712b3b1c4df39c4099ba73f961c
egi rizki ramadani <https://gitlab.com/egi321910004/basic_git.git>
2022-09-08 23:15:35 +0700

    Commit sesi7

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TimerClass from "./components/TimerClass";
+import TimerFunc from "./components/TimerFunc";
+
+export default function App() {
+  return (
+    <div>
+      <TimerClass />
+      <TimerFunc />
+    </div>
+  );
+}
